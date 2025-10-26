84fd459fb229ce3b36ed31f36104b3561c24da5a
zhaofei <laczfyn@gmail.com>
2025-10-26 17:27:32 +0800

    modify signal name for chip.

--- rtl/top/chip.v
+++ rtl/top/chip.v
@@ -57,72 +57,72 @@ module chip (
 );
 
     wire [`WordDataBus]    m_rd_data;
-    wire                   m_rdy_;
+    wire                   m_rdy_n;
 
-    wire                   m0_req_;
+    wire                   m0_req_n;
     wire [`WordAddrBus]    m0_addr;
-    wire                   m0_as_;
+    wire                   m0_as_n;
     wire                   m0_rw;
     wire [`WordDataBus]    m0_wr_data;
-    wire                   m0_grnt_;
+    wire                   m0_grnt_n;
 
-    wire                   m1_req_;
+    wire                   m1_req_n;
     wire [`WordAddrBus]    m1_addr;
-    wire                   m1_as_;
+    wire                   m1_as_n;
     wire                   m1_rw;
     wire [`WordDataBus]    m1_wr_data;
-    wire                   m1_grnt_;
+    wire                   m1_grnt_n;
 
-    wire                   m2_req_;
+    wire                   m2_req_n;
     wire [`WordAddrBus]    m2_addr;
-    wire                   m2_as_;
+    wire                   m2_as_n;
     wire                   m2_rw;
     wire [`WordDataBus]    m2_wr_data;
-    wire                   m2_grnt_;
+    wire                   m2_grnt_n;
 
-    wire                   m3_req_;
+    wire                   m3_req_n;
     wire [`WordAddrBus]    m3_addr;
-    wire                   m3_as_;
+    wire                   m3_as_n;
     wire                   m3_rw;
     wire [`WordDataBus]    m3_wr_data;
-    wire                   m3_grnt_;
+    wire                   m3_grnt_n;
 
     wire [`WordAddrBus]    s_addr;
-    wire                   s_as_;
+    wire                   s_as_n;
     wire                   s_rw;
     wire [`WordDataBus]    s_wr_data;
 
     wire [`WordDataBus]    s0_rd_data;
-    wire                   s0_rdy_;
-    wire                   s0_cs_;
+    wire                   s0_rdy_n;
+    wire                   s0_cs_n;
 
     wire [`WordDataBus]    s1_rd_data;
-    wire                   s1_rdy_;
-    wire                   s1_cs_;
+    wire                   s1_rdy_n;
+    wire                   s1_cs_n;
 
     wire [`WordDataBus]    s2_rd_data;
-    wire                   s2_rdy_;
-    wire                   s2_cs_;
+    wire                   s2_rdy_n;
+    wire                   s2_cs_n;
 
     wire [`WordDataBus]    s3_rd_data;
-    wire                   s3_rdy_;
-    wire                   s3_cs_;
+    wire                   s3_rdy_n;
+    wire                   s3_cs_n;
 
     wire [`WordDataBus]    s4_rd_data;
-    wire                   s4_rdy_;
-    wire                   s4_cs_;
+    wire                   s4_rdy_n;
+    wire                   s4_cs_n;
 
     wire [`WordDataBus]    s5_rd_data;
-    wire                   s5_rdy_;
-    wire                   s5_cs_;
+    wire                   s5_rdy_n;
+    wire                   s5_cs_n;
 
     wire [`WordDataBus]    s6_rd_data;
-    wire                   s6_rdy_;
-    wire                   s6_cs_;
+    wire                   s6_rdy_n;
+    wire                   s6_cs_n;
 
     wire [`WordDataBus]    s7_rd_data;
-    wire                   s7_rdy_;
-    wire                   s7_cs_;
+    wire                   s7_rdy_n;
+    wire                   s7_cs_n;
 
     wire                   irq_timer;
     wire                   irq_uart_rx;
@@ -139,20 +139,20 @@ module chip (
 
         // IF Stage
         .if_bus_rd_data_i      (m_rd_data),
-        .if_bus_rdy_n_i        (m_rdy_),
-        .if_bus_grnt_n_i       (m0_grnt_),
-        .if_bus_req_n_o        (m0_req_),
+        .if_bus_rdy_n_i        (m_rdy_n),
+        .if_bus_grnt_n_i       (m0_grnt_n),
+        .if_bus_req_n_o        (m0_req_n),
         .if_bus_addr_o         (m0_addr),
-        .if_bus_as_n_o         (m0_as_),
+        .if_bus_as_n_o         (m0_as_n),
         .if_bus_rw_o           (m0_rw),
         .if_bus_wr_data_o      (m0_wr_data),
         // MEM Stage
         .mem_bus_rd_data_i     (m_rd_data),
-        .mem_bus_rdy_n_i       (m_rdy_),
-        .mem_bus_grnt_n_i      (m1_grnt_),
-        .mem_bus_req_n_o       (m1_req_),
+        .mem_bus_rdy_n_i       (m_rdy_n),
+        .mem_bus_grnt_n_i      (m1_grnt_n),
+        .mem_bus_req_n_o       (m1_req_n),
         .mem_bus_addr_o        (m1_addr),
-        .mem_bus_as_n_o        (m1_as_),
+        .mem_bus_as_n_o        (m1_as_n),
         .mem_bus_rw_o          (m1_rw),
         .mem_bus_wr_data_o     (m1_wr_data),
 
@@ -160,29 +160,29 @@ module chip (
     );
 
     /* 暂未使用 */
-    assign m2_addr      = `WORD_ADDR_W'h0;
-    assign m2_as_       = `DISABLE_N;
-    assign m2_rw        = `READ;
-    assign m2_wr_data   = `WORD_DATA_W'h0;
-    assign m2_req_      = `DISABLE_N;
+    assign m2_addr       = `WORD_ADDR_W'h0;
+    assign m2_as_n       = `DISABLE_N;
+    assign m2_rw         = `READ;
+    assign m2_wr_data    = `WORD_DATA_W'h0;
+    assign m2_req_n      = `DISABLE_N;
 
     /* 暂未使用 */
-    assign m3_addr      = `WORD_ADDR_W'h0;
-    assign m3_as_       = `DISABLE_N;
-    assign m3_rw        = `READ;
-    assign m3_wr_data   = `WORD_DATA_W'h0;
-    assign m3_req_      = `DISABLE_N;
+    assign m3_addr       = `WORD_ADDR_W'h0;
+    assign m3_as_n       = `DISABLE_N;
+    assign m3_rw         = `READ;
+    assign m3_wr_data    = `WORD_DATA_W'h0;
+    assign m3_req_n      = `DISABLE_N;
 
     /********** ROM **********/
     rom u_rom (
         .clk           (clk),
         .reset         (reset),
 
-        .cs_n_i        (s0_cs_),
-        .as_n_i        (s_as_),
+        .cs_n_i        (s0_cs_n),
+        .as_n_i        (s_as_n),
         .addr_i        (s_addr[`RomAddrLoc]),
         .rd_data_o     (s0_rd_data),
-        .rdy_n_o       (s0_rdy_)
+        .rdy_n_o       (s0_rdy_n)
     );
 
 `ifdef IMPLEMENT_PE
@@ -200,18 +200,18 @@ module chip (
         .clk(clk),
         .reset(reset),
 
-        .cs_(s1_cs_),
-        .as_(s_as_),
+        .cs_(s1_cs_n),
+        .as_(s_as_n),
         .rw(s_rw),
         .addr(s_addr),
         .wr_data(s_wr_data),
         .rd_data(s1_rd_data),
-        .rdy_(s1_rdy_)
+        .rdy_(s1_rdy_n)
     );
 `else
     /* 暂未使用 */
     assign s1_rd_data   = `WORD_DATA_W'h0;
-    assign s1_rdy_      = `DISABLE_N;
+    assign s1_rdy_n      = `DISABLE_N;
 `endif
 
 `ifdef IMPLEMENT_TIMER
@@ -220,19 +220,19 @@ module chip (
         .clk             (clk),
         .reset           (reset),
 
-        .cs_n_i          (s2_cs_),
-        .as_n_i          (s_as_),
+        .cs_n_i          (s2_cs_n),
+        .as_n_i          (s_as_n),
         .rw_i            (s_rw),
         .addr_i          (s_addr[`TimerAddrLoc]),
         .wr_data_i       (s_wr_data),
         .rd_data_o       (s2_rd_data),
-        .rdy_n_o         (s2_rdy_),
+        .rdy_n_o         (s2_rdy_n),
 
         .irq_o           (irq_timer)
      );
 `else
     assign s2_rd_data = `WORD_DATA_W'h0;
-    assign s2_rdy_    = `DISABLE_N;
+    assign s2_rdy_n    = `DISABLE_N;
     assign irq_timer  = `DISABLE;
 `endif
 
@@ -242,13 +242,13 @@ module chip (
         .clk               (clk),
         .reset             (reset),
 
-        .cs_n_i            (s3_cs_),
-        .as_n_i            (s_as_),
+        .cs_n_i            (s3_cs_n),
+        .as_n_i            (s_as_n),
         .rw_i              (s_rw),
         .addr_i            (s_addr[`UartAddrLoc]),
         .wr_data_i         (s_wr_data),
         .rd_data_o         (s3_rd_data),
-        .rdy_n_o           (s3_rdy_),
+        .rdy_n_o           (s3_rdy_n),
 
         .irq_rx_o          (irq_uart_rx),
         .irq_tx_o          (irq_uart_tx),
@@ -258,7 +258,7 @@ module chip (
     );
 `else
     assign s3_rd_data  = `WORD_DATA_W'h0;
-    assign s3_rdy_       = `DISABLE_N;
+    assign s3_rdy_n       = `DISABLE_N;
     assign irq_uart_rx = `DISABLE;
     assign irq_uart_tx = `DISABLE;
 `endif
@@ -269,13 +269,13 @@ module chip (
         .clk             (clk),
         .reset           (reset),
 
-        .cs_n_i          (s4_cs_),
-        .as_n_i          (s_as_),
+        .cs_n_i          (s4_cs_n),
+        .as_n_i          (s_as_n),
         .rw_i            (s_rw),
         .addr_i          (s_addr[`GpioAddrLoc]),
         .wr_data_i       (s_wr_data),
         .rd_data_i       (s4_rd_data),
-        .rdy_n_o         (s4_rdy_)
+        .rdy_n_o         (s4_rdy_n)
 
 `ifdef GPIO_IN_CH
         , .gpio_in       (gpio_in)
@@ -289,7 +289,7 @@ module chip (
     );
 `else
     assign s4_rd_data   = `WORD_DATA_W'h0;
-    assign s4_rdy_      = `DISABLE_N;
+    assign s4_rdy_n      = `DISABLE_N;
 `endif
 
 `ifdef IMPLEMENT_SPI
@@ -302,12 +302,12 @@ module chip (
         .clk           (clk),
         .rst_n         (reset == `RESET_DISABLE ? 1'b1 : 1'b0),
 
-        .req_i         (s5_cs_),
+        .req_i         (s5_cs_n),
         .we_i          (s_rw),
         .addr_i        ({{(32-`WORD_ADDR_W){1'b0}}, s_addr}),
         .data_in_i     (s_wr_data),
         .data_out_o    (s5_rd_data),
-        .ack_o         (s5_rdy_),
+        .ack_o         (s5_rdy_n),
 
         .spi_cs_n_o    (spi_cs_n),
         .spi_clk_o     (spi_clk),
@@ -317,7 +317,7 @@ module chip (
 `else
     /* 暂未使用 */
     assign s5_rd_data   = `WORD_DATA_W'h0;
-    assign s5_rdy_      = `DISABLE_N;
+    assign s5_rdy_n      = `DISABLE_N;
     assign spi_cs_n     = 1'b1;
     assign spi_clk      = 1'b0;
     assign spi_mosi     = 1'b0;
@@ -325,7 +325,7 @@ module chip (
 
     /* 暂未使用 */
     assign s6_rd_data   = `WORD_DATA_W'h0;
-    assign s6_rdy_      = `DISABLE_N;
+    assign s6_rdy_n      = `DISABLE_N;
 
 `ifdef IMPLEMENT_JTAG
     /********** JTAG **********/
@@ -343,19 +343,19 @@ module chip (
         .tdo_o           (tdo),
         .tdo_en_o        (tdo_en),
 
-        .req_i           (s7_cs_),
+        .req_i           (s7_cs_n),
         .we_i            (s_rw),
         .addr_i          ({{(64-`WORD_ADDR_W){1'b0}}, s_addr}),
         .data_in_i       (s_wr_data),
         .data_out_o      (s7_rd_data[`WORD_DATA_W-1:0]),
-        .ack_o           (s7_rdy_),
+        .ack_o           (s7_rdy_n),
 
         .debug_data_o    (),
         .debug_valid_o   ()
     );
 `else
     assign s7_rd_data    = `WORD_DATA_W'h0;
-    assign s7_rdy_       = `DISABLE_N;
+    assign s7_rdy_n       = `DISABLE_N;
     assign tdo           = `LOW;
     assign tdo_en        = `LOW;
 `endif
@@ -366,72 +366,72 @@ module chip (
         .reset             (reset),
 
         .m_rd_data_o       (m_rd_data),
-        .m_rdy_n_o         (m_rdy_),
+        .m_rdy_n_o         (m_rdy_n),
 
-        .m0_req_n_i         (m0_req_),
+        .m0_req_n_i         (m0_req_n),
         .m0_addr_i          (m0_addr),
-        .m0_as_n_i          (m0_as_),
+        .m0_as_n_i          (m0_as_n),
         .m0_rw_i            (m0_rw),
         .m0_wr_data_i       (m0_wr_data),
-        .m0_grnt_n_o        (m0_grnt_),
+        .m0_grnt_n_o        (m0_grnt_n),
 
-        .m1_req_n_i         (m1_req_),
+        .m1_req_n_i         (m1_req_n),
         .m1_addr_i          (m1_addr),
-        .m1_as_n_i          (m1_as_),
+        .m1_as_n_i          (m1_as_n),
         .m1_rw_i            (m1_rw),
         .m1_wr_data_i       (m1_wr_data),
-        .m1_grnt_n_o        (m1_grnt_),
+        .m1_grnt_n_o        (m1_grnt_n),
 
-        .m2_req_n_i         (m2_req_),
+        .m2_req_n_i         (m2_req_n),
         .m2_addr_i          (m2_addr),
-        .m2_as_n_i          (m2_as_),
+        .m2_as_n_i          (m2_as_n),
         .m2_rw_i            (m2_rw),
         .m2_wr_data_i       (m2_wr_data),
-        .m2_grnt_n_o        (m2_grnt_),
+        .m2_grnt_n_o        (m2_grnt_n),
 
-        .m3_req_n_i         (m3_req_),
+        .m3_req_n_i         (m3_req_n),
         .m3_addr_i          (m3_addr),
-        .m3_as_n_i          (m3_as_),
+        .m3_as_n_i          (m3_as_n),
         .m3_rw_i            (m3_rw),
         .m3_wr_data_i       (m3_wr_data),
-        .m3_grnt_n_o        (m3_grnt_),
+        .m3_grnt_n_o        (m3_grnt_n),
 
         .s_addr_o           (s_addr),
-        .s_as_n_o           (s_as_),
+        .s_as_n_o           (s_as_n),
         .s_rw_o             (s_rw),
         .s_wr_data_o        (s_wr_data),
 
         .s0_rd_data_i       (s0_rd_data),
-        .s0_rdy_n_i         (s0_rdy_),
-        .s0_cs_n_o          (s0_cs_),
+        .s0_rdy_n_i         (s0_rdy_n),
+        .s0_cs_n_o          (s0_cs_n),
 
         .s1_rd_data_i       (s1_rd_data),
-        .s1_rdy_n_i         (s1_rdy_),
-        .s1_cs_n_o          (s1_cs_),
+        .s1_rdy_n_i         (s1_rdy_n),
+        .s1_cs_n_o          (s1_cs_n),
 
         .s2_rd_data_i       (s2_rd_data),
-        .s2_rdy_n_i         (s2_rdy_),
-        .s2_cs_n_o          (s2_cs_),
+        .s2_rdy_n_i         (s2_rdy_n),
+        .s2_cs_n_o          (s2_cs_n),
 
         .s3_rd_data_i       (s3_rd_data),
-        .s3_rdy_n_i         (s3_rdy_),
-        .s3_cs_n_o          (s3_cs_),
+        .s3_rdy_n_i         (s3_rdy_n),
+        .s3_cs_n_o          (s3_cs_n),
 
         .s4_rd_data_i       (s4_rd_data),
-        .s4_rdy_n_i         (s4_rdy_),
-        .s4_cs_n_o          (s4_cs_),
+        .s4_rdy_n_i         (s4_rdy_n),
+        .s4_cs_n_o          (s4_cs_n),
 
         .s5_rd_data_i       (s5_rd_data),
-        .s5_rdy_n_i         (s5_rdy_),
-        .s5_cs_n_o          (s5_cs_),
+        .s5_rdy_n_i         (s5_rdy_n),
+        .s5_cs_n_o          (s5_cs_n),
 
         .s6_rd_data_i       (s6_rd_data),
-        .s6_rdy_n_i         (s6_rdy_),
-        .s6_cs_n_o          (s6_cs_),
+        .s6_rdy_n_i         (s6_rdy_n),
+        .s6_cs_n_o          (s6_cs_n),
 
         .s7_rd_data_i       (s7_rd_data),
-        .s7_rdy_n_i         (s7_rdy_),
-        .s7_cs_n_o          (s7_cs_)
+        .s7_rdy_n_i         (s7_rdy_n),
+        .s7_cs_n_o          (s7_cs_n)
     );
 
 endmodule
