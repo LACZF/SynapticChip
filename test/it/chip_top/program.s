5fef2f5d461fbdfb23c886f85675d08bbd9e33c8
LACZF <365699746@qq.com>
2025-11-19 08:35:47 +0800

    set PE to 16*16 and 2K ram.

--- test/it/chip_top/program.s
+++ test/it/chip_top/program.s
@@ -65,8 +65,8 @@
 .equ PE_HIGH_BW_READ_ADDR,  PE_TOP_BASE + 0x100010   # 高带宽读取地址
 
 # PE阵列尺寸定义
-.equ PE_ARRAY_X,            8                          # PE阵列X方向尺寸
-.equ PE_ARRAY_Y,            8                          # PE阵列Y方向尺寸
+.equ PE_ARRAY_X,            16                         # PE阵列X方向尺寸
+.equ PE_ARRAY_Y,            16                         # PE阵列Y方向尺寸
 .equ PE_TOTAL_COUNT,        PE_ARRAY_X * PE_ARRAY_Y    # PE总数
 
 # PE内存映射 - 操作数和配置存储在PE_mem中
@@ -113,7 +113,7 @@
 .equ PE_SRC_OPERAND2,  0x6     # 外部操作数2 (3'b110)
 
 # 栈指针初始地址
-.equ STACK_TOP,      RAM_BASE + 0x1000
+.equ STACK_TOP,      RAM_BASE + 0x400
 
 # 中断向量表
 # RISC-V中断向量表索引规则：
