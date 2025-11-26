02efd6a2f17f179e771ca63f7d301fff69573bb4
LACZF <365699746@qq.com>
2025-11-26 21:12:13 +0800

    fix bug for clk_gen.

--- rtl/common/clk_gen.v
+++ rtl/common/clk_gen.v
@@ -10,15 +10,18 @@ module clk_gen (
     parameter CNT_WIDTH      = 16;
 
     reg [CNT_WIDTH-1:0] div_cnt;
+    reg                 div_clk;
 
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             div_cnt <= {CNT_WIDTH{1'b0}};
+            div_clk <= 1'b0;
         end else begin
-            div_cnt <= div_cnt + 1'b1;
+            div_cnt <= (div_cnt >= (div_i - 1)) ? 0 : div_cnt + 1'b1;
+            div_clk <= (div_cnt >= (div_i - 1)) ? ~div_clk : div_clk;
         end
     end
 
-    assign clk_o = ((div_i > 1) ? div_cnt[div_i-1] : clk);
+    assign clk_o = div_clk;
 
 endmodule
